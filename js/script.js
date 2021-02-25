33ad69b332c1eb46125eea7018a168ca8f94cde0
Taylan Mun <taylanmun@gmail.com>
2021-02-25 20:06:01 +0300

    finish exceeds expectations

--- js/script.js
+++ js/script.js
@@ -7,8 +7,30 @@ FSJS Project 2 - Data Pagination and Filtering
 // number of students to display on the page
 const pageSize = 9;
 
+// initial page number
+let page = 1;
+
 // pagination html
 const paginationUL = document.querySelector('.link-list');
+
+// using for pagination if search input not empty
+let filterStudentArr = [];
+
+/**
+ * Create html element
+ * 
+ * @param {string} elementName 
+ * @param {string} property 
+ * @param {string} value 
+ * @param {string} text 
+ */
+function createElement(elementName, property = null, value = null, text = null) {
+   const element = document.createElement(elementName);
+   element[property] = value;
+   element.textContent = text;
+   return element;
+}
+
 
 
 /**
@@ -21,16 +43,51 @@ function showPage (studentsList, page) {
    const endIndex = (page * pageSize);
    const studentListUL = document.querySelector('.student-list');
    studentListUL.innerHTML = '';
-
-   for (let i = 0; i < studentsList.length; i++) {
+   if(studentsList.length){
+      for (let i = 0; i < studentsList.length; i++) {
          if(i >= startIndex && i < endIndex){
             const studentData = studentsList[i];
             studentListUL.insertAdjacentHTML('beforeend', createStudent(studentData))
          }  
+      }
+   } else {
+      const noFoundStudentLI = createElement('li');
+      const noFoundtStudentP = createElement('p',null,null,'No Students Found...');
+      noFoundtStudentP.style.color = 'red';
+      noFoundStudentLI.appendChild(noFoundtStudentP);
+      studentListUL.insertAdjacentHTML('beforeend', noFoundStudentLI.outerHTML)
+
    }
+
 }
 
-1
+/**
+ * Create search element for show page
+ *
+ */
+function createSearchElement(){
+   const header = document.querySelector('.header');
+
+   const label = createElement('label', 'for', 'search');
+   label.className = 'student-search';
+
+   const input = createElement('input', 'placeholder', 'Search by name...');
+   input.id = 'search';
+
+   const button = createElement('button', 'type', 'button');
+   const img = createElement('img', 'src', 'img/icn-search.svg');
+   button.appendChild(img);
+
+   label.appendChild(input);
+   label.appendChild;
+   
+   header.insertAdjacentHTML("beforeend", label.outerHTML);
+}
+
+/**
+ * create student html element for showPage function.
+ * @param {Object} student - for take student information.
+ */
 function createStudent(student){
    return `<li class="student-item cf">
    <div class="student-details">
@@ -48,41 +105,64 @@ function createStudent(student){
  </li>`;
 }
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
 /**
  * Show Pagination for showPage function.
  * @param {Array} studentsList - for take students count.
  */
 function addPagination(studentsList) {
-   const numberOfPage = Math.round(studentsList.length / pageSize);
+   const numberOfPage = Math.ceil(studentsList.length / pageSize);
    paginationUL.innerHTML = '';
-
    for (let i = 1; i <= numberOfPage; i++) {
-      const paginationElement = ` 
-         <li>
-            <button type="button">${i}</button>
-         </li>`
-      paginationUL.insertAdjacentHTML('beforeend', paginationElement);
+      const paginationLi = createElement('li');
+      const paginationButton = createElement('button', 'type', 'button', i);
+      paginationLi.appendChild(paginationButton);
+      paginationUL.insertAdjacentHTML('beforeend', paginationLi.outerHTML);
    }
-   paginationUL.firstElementChild.firstElementChild.className = 'active';
+   if(numberOfPage != 0){
+      paginationUL.firstElementChild.firstElementChild.className = 'active';
+   }
 }
+
+// initial page to appear when the page loads
+showPage(data, page);
+addPagination(data)
+createSearchElement()
+
+
+function filterItems(students, query) {
+   if (query) {
+       return students.filter(el =>
+           el.name.first.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+           el.name.last.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+   }
+   return null;
+}
+
+
+// take search input for filter
+const searchInputElement = document.getElementById('search');
+
+// filtering data 
+searchInputElement.addEventListener('input', (e) => {
+   const filter = e.target.value.toLowerCase();
+   if(filter){
+      filterStudentArr = data.filter(student =>
+         (student.name.first + ' ' + student.name.last).toLowerCase().indexOf(filter) !== -1);
+         showPage(filterStudentArr, 1);
+         addPagination(filterStudentArr)
+   }
+});
 
 // change studentlist data when button clicking and change active class
 paginationUL.addEventListener('click', (e) => {
    if(e.target.tagName === 'BUTTON'){
       paginationUL.getElementsByClassName('active')[0].className ='';
       e.target.className = "active";
-      showPage(data, e.target.textContent);
+      page = parseInt(e.target.textContent);
+      if(!searchInputElement.value){
+         showPage(data, page);
+      }else{
+         showPage(filterStudentArr, page);
+      }
    }
-})
-
-// Call functions
-
-
-// initial page to appear when the page loads
-showPage(data, 1);
-addPagination(data)
+});
